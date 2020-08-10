33d49e0d44035e621ee1d5b83db76902bc28600e
Antonio Lobato <aj.lobato@gmail.com>
2020-08-10 04:13:51 +0000

    More work on the Spanner backend

--- backends/spanner.go
+++ backends/spanner.go
@@ -2,10 +2,13 @@ package backends
 
 import (
 	"context"
+	"time"
 
 	"cloud.google.com/go/spanner"
 	pb "github.com/gcp-services/lock/storage"
+	"github.com/golang/protobuf/ptypes"
 	"google.golang.org/api/option"
+	"google.golang.org/grpc/codes"
 )
 
 // Spanner is an implementation of the Lock server that uses Spanner as a backing store.
@@ -28,6 +31,36 @@ func NewSpanner(ctx context.Context, database string, opts ...option.ClientOptio
 // TryLock will attempt to acquire a lock. If a lock can not be acquired, the function
 // will return immediately with the reason for lock acquisition failure.
 func (s *Spanner) TryLock(ctx context.Context, in *pb.TryLockRequest) (*pb.TryLockResponse, error) {
+	if _, err := s.client.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		row, err := txn.ReadRow(ctx, "Locks", spanner.Key{"uuid"}, []string{"owner", "expires"})
+		switch {
+		case spanner.ErrCode(err) == codes.NotFound:
+			// TODO(lobato): apply lock
+			break
+		case err != nil:
+			return err
+		}
+
+		// A lock entry was found, validate it.
+		readLock := pb.Lock{}
+		row.Columns(&readLock.Uuid, &readLock.Owner, &readLock.Expires)
+		expire, err := ptypes.Timestamp(readLock.Expires)
+		if err != nil {
+			return err
+		}
+
+		// Check if this lock is expired and has not been refreshed. Claim this lock
+		// if the lock has expired.
+		if time.Now().After(expire) {
+			// TODO(lobato): apply lock
+			return nil
+		}
+
+		return ErrLockBusy
+	}); err != nil {
+		return nil, err
+	}
+
 	return nil, nil
 }
 
